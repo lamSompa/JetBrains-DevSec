fee35b1de4342d83c7340e478edb0d40b2046f3d
Abeiku Sompa Nyarko-Lartey <abeikusompanyarkolartey@gmail.com>
2025-10-05 11:38:07 +0200

    Added tests and update README

---
+++ tests/test_detector.py
@@ -0,0 +1,6 @@
+from jetbrains_dev_sec import detect_secrets
+
+def test_detect_fake_aws_key():
+    code = 'aws_key = "AKIA1234567890EXAMPLE"'
+    findings = detect_secrets(code)
+    assert any("AKIA" in finding["value"] for finding in findings)
